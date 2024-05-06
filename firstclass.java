f84166d3eb0dfa8c123aca3b5628bebea53c1f60
itsdevaditya <adigoyl05@gmail.com>
2024-05-06 21:13:41 +0530

    Add java programm

---
+++ firstclass.java
@@ -0,0 +1,10 @@
+class firstclass{
+    // function void main()
+    public static void main(String args[]){
+        System.out.print("hello world with java\n");
+        System.out.println("hello world with java");
+        // System --- class.out.
+        // println-- function
+        // println --- give next line
+    }
+}
---
+++ variable.java
@@ -0,0 +1,8 @@
+ public class variable {
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 43;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
