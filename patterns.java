9ad6cb609e6601b5f51c36b5cde97f9cddc4d778
itsdevaditya <adigoyl05@gmail.com>
2024-05-08 18:00:37 +0530

    first pattern

---
+++ patterns.java
@@ -0,0 +1,23 @@
+public class patterns {
+    public static void main(String[] args) {
+        for(int i = 0 ; i <5 ; i++)
+        {
+            System.out.print("* ");
+        }
+        System.out.print("\n");
+        for(int i = 0 ; i <5 ; i++)
+        {
+            System.out.print("* ");
+        }
+        System.out.print("\n");
+        for(int i = 0 ; i <5 ; i++)
+        {
+            System.out.print("* ");
+        }
+        System.out.print("\n");
+        for(int i = 0 ; i <5 ; i++)
+        {
+            System.out.print("* ");
+        }
+    }
+}
