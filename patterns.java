2aaa6028c097e4d8b7f620db13e8116263655114
itsdevaditya <adigoyl05@gmail.com>
2024-05-08 20:35:55 +0530

    correct my code

--- patterns.java
+++ patterns.java
@@ -2,22 +2,12 @@ public class patterns {
     public static void main(String[] args) {
         for(int i = 0 ; i <5 ; i++)
         {
-            System.out.print("* ");
-        }
-        System.out.print("\n");
-        for(int i = 0 ; i <5 ; i++)
-        {
-            System.out.print("* ");
-        }
-        System.out.print("\n");
-        for(int i = 0 ; i <5 ; i++)
-        {
-            System.out.print("* ");
-        }
-        System.out.print("\n");
-        for(int i = 0 ; i <5 ; i++)
-        {
-            System.out.print("* ");
-        }
+            for(int j = 0 ; j <5 ; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }        
     }
 }
