97e4a47e58f2968e23e0717741d2dcba62829293
itsdevaditya <adigoyl05@gmail.com>
2024-05-10 11:05:43 +0530

    very difficult code

--- patterns/pattern6.java
+++ patterns/pattern6.java
@@ -9,11 +9,10 @@ public class pattern6 {
         }
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i + 1; j++) {
-
                 System.out.print(j);
-
             }
             System.out.println();
         }
+        
     }
 }
