71a000bf3fee4aa754a95668d30a2d908ef7c643
itsdevaditya <adigoyl05@gmail.com>
2024-05-13 12:56:31 +0530

    Success in butterfly program

--- patterns/pattern9.java
+++ patterns/pattern9.java
@@ -4,7 +4,24 @@ public class pattern9 {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
-                
+            }
+            for (int j = 1; j <= 2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2*(i-1); j++) {
+                System.out.print(" ");
+            }
+            for (int j = n; j >= i; j--) {
+                System.out.print("*");
             }
             System.out.println();
         }
