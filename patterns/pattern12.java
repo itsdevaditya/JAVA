dbe57867b4135e0e536dba84f6ecaa6819668f9c
itsdevaditya <adigoyl05@gmail.com>
2024-05-15 10:23:48 +0530

    palindrom try

--- patterns/pattern12.java
+++ patterns/pattern12.java
@@ -1,4 +1,4 @@
-public class pattern11 {
+public class pattern12 {
     public static void main(String[] args) {
         int n = 5;
 
@@ -8,18 +8,12 @@ public class pattern11 {
             }
             int m = (2*i)-1;
             for (int j = 1; j <= m; j++) {
-                if (j % 2 != 0)
+                if ( j == m || i == m || j == 1)
                     System.out.print(i);
                 else
-                    System.out.print(" ");
-
-            }
-            for (int j = 1; j <= (n - i); j++) {
-                System.out.print(" ");
+                    System.out.print( j );
             }
             System.out.println();
         }
     }
 }
-
-
