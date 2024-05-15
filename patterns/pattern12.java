3655219813a849d3e9731df1bfa47be25bb61a70
itsdevaditya <adigoyl05@gmail.com>
2024-05-15 11:32:25 +0530

    Success in making palindrone

--- patterns/pattern12.java
+++ patterns/pattern12.java
@@ -6,13 +6,23 @@ public class pattern12 {
             for (int j = 1; j <= (n - i); j++) {
                 System.out.print(" ");
             }
-            int m = (2*i)-1;
-            for (int j = 1; j <= m; j++) {
-                if ( j == m || i == m || j == 1)
-                    System.out.print(i);
-                else
-                    System.out.print( j );
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i - j + 1);
             }
+            for (int j = 1; j <= i; j++) {
+                if (j == 1) {
+                    System.out.print("");
+                } else {
+                    System.out.print(j);
+                }
+            }
+            // int m = (2*i)-1;
+            // for (int j = 1; j <= m; j++) {
+            // if ( j == m || i == m || j == 1)
+            // System.out.print(i);
+            // else
+            // System.out.print( j );
+            // }
             System.out.println();
         }
     }
