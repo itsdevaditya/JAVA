c8994b5d28d28f411101c32a0662b5a0fa0d7445
itsdevaditya <adigoyl05@gmail.com>
2024-05-17 20:25:30 +0530

    first array

---
+++ Array/Array.java
@@ -0,0 +1,14 @@
+package JAVA.Array;
+
+public class Array {
+    public static void main(String[] args) {
+        int [] marks = new int[3];
+        marks[0] = 97;
+        marks[1] = 94;
+        marks[2] = 98;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+    
+}
