413cdaddef684fb47aa01521b14079117064a845
itsdevaditya <adigoyl05@gmail.com>
2024-05-18 12:05:56 +0530

    2d Array

---
+++ Array/Array_2d.java
@@ -0,0 +1,26 @@
+package JAVA.Array;
+
+import java.util.Scanner;
+
+public class Array_2d {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int colms = sc.nextInt();
+        int[][] numbers = new int[rows][colms];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < colms; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < colms; j++) {
+               System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
