ad43f31d85f81e14735a04cbcb8ceea5e1b2d2bc
itsdevaditya <adigoyl05@gmail.com>
2024-05-19 16:49:12 +0530

    Substring function

--- Learn_String/Strings.java
+++ Learn_String/Strings.java
@@ -11,21 +11,24 @@ public class Strings {
         System.out.println("Your namer is : " + first_name + " " + last_name);
         // Length function
         System.out.println((first_name + " " + last_name).length());
-        //  charAt
-        for(int i = 0 ; i < first_name.length() ; i ++){
+        // charAt
+        for (int i = 0; i < first_name.length(); i++) {
             System.out.println(first_name.charAt(i));
         }
         // String1.compareTo(String2)
         // if s1 < s2 = +ve
         // if s1 == s2 = 0
         // if s1 - s2 = -ve
-        
+
         if (first_name.compareTo(last_name) == 0) {
             System.out.println("First name and last name is equal ");
-            
+
+        } else {
+            System.out.println("First name and last name are not equal ");
         }
-        else
-        System.out.println("First name and last name are not equal ");
+        // substring function
+        // Stings are Immuatable -- We can not change or modify any string 
+        System.out.println(first_name.substring(3));
     }
 
 }
