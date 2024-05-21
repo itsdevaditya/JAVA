1a388c19e5e58de9c0fce30bd55e0b95dd4a4260
itsdevaditya <adigoyl05@gmail.com>
2024-05-21 22:12:26 +0530

    external iteration

---
+++ function and method/lambda.java
@@ -0,0 +1,19 @@
+
+// Java program to demonstrate an
+// external iterator
+import java.util.Arrays;
+import java.util.List;
+public class lambda {
+    public static void main(String[] args)
+    {
+        List<Integer> numbers
+            = Arrays.asList(11, 22, 33, 44,
+                            55, 66, 77, 88,
+                            99, 100);
+ 
+        // External iterator, for Each loop
+        for (Integer n : numbers) {
+            System.out.print(n + " ");
+        }
+    }
+}
