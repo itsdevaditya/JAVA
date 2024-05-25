44d49a718332ae6b269313c73a66068f9a5b9f01
itsdevaditya <adigoyl05@gmail.com>
2024-05-25 08:49:21 +0530

    Array organization

--- arrays/find_number.java
+++ arrays/find_number.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class find_number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of Array : ");
         int n = sc.nextInt();
 
         int number[] = new int[n];
         for (int i = 0; i < n; i++) {
+            System.out.print("Enter " + i + " number : ");
             number[i] = sc.nextInt();
         }
         for (int i = 0; i <= n; i++) {
             int x = 4;
             if (number[i] == x) {
-                System.out.println(x + " found at index " +i);
+                System.out.println(x + " found at index " + i);
 
-            } 
-            // else
-            //     System.out.println(x + " is not present in this array");
+            }
         }
     }
 }
