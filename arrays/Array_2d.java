74a1b4d6a1faef5308c68e03370e59cd5fff6b7c
itsdevaditya <adigoyl05@gmail.com>
2024-05-27 17:11:02 +0530

    show 2D array

--- arrays/Array_2d.java
+++ arrays/Array_2d.java
@@ -1,14 +1,19 @@
 package arrays;
+
 import java.util.Scanner;
+
 public class Array_2d {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows : ");
         int rows = sc.nextInt();
+        System.out.print("Enter number of columns : ");
         int colms = sc.nextInt();
         int[][] numbers = new int[rows][colms];
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < colms; j++) {
+                System.out.print("Enter ("+i + ", " +j+ ") : ");
                 numbers[i][j] = sc.nextInt();
             }
         }
