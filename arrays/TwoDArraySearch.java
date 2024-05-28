a7ebc7a41b05cf7acc9fff697eb1a8bdfccb26d9
itsdevaditya <adigoyl05@gmail.com>
2024-05-28 20:47:16 +0530

    arrange for linkdin

--- arrays/TwoDArraySearch.java
+++ arrays/TwoDArraySearch.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class TwoDArraySearch {
     public static void main(String[] args) {
+        // Learn to create and show 2D Array
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows : ");
         int rows = sc.nextInt();
+        System.out.print("Enter number of columns : ");
         int colms = sc.nextInt();
         int[][] numbers = new int[rows][colms];
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < colms; j++) {
+                System.out.print("Enter (" + i + ", " + j + ") : ");
                 numbers[i][j] = sc.nextInt();
             }
         }
