68064421e6a24164f6d91321f36a9aeb9f1930e8
itsdevaditya <adigoyl05@gmail.com>
2024-05-29 20:44:57 +0530

    Get operation

--- Operators_binary_number/bit_operation_Get.java
+++ Operators_binary_number/bit_operation_Get.java
@@ -3,6 +3,9 @@ package Operators_binary_number;
 public class bit_operation_Get {
     public static void main(String[] args) {
         int num = 5;
+        // find 2nd bit of 5 in binary number 
+        // 5 == 0101 
+ //possition == 3210
         int pos = 2;
         int bitmas = 1 << 2;
         if ((bitmas & num) == 0) {
