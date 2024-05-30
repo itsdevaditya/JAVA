2240aca1e9c0001480714129a82f5f1a642342f9
itsdevaditya <adigoyl05@gmail.com>
2024-05-30 23:06:37 +0530

    print function

--- Shorting/insertion_sort.java
+++ Shorting/insertion_sort.java
@@ -1,7 +1,16 @@
 package Shorting;
 
 public class insertion_sort {
-    public static void main(String[] args) {
-        
+    public static void printArray(int arry[]) {
+        for (int i = 0; i < arry.length; i++) {
+            System.out.print(arry[i] + " ");
+        }
     }
+    public static void main(String[] args) {
+        // sorting using selection sort
+
+        int[] arr = { 4, 2, 5, 27, 1 };
+        System.out.println("unsorted array : ");
+        printArray(arr);}
+  
 }
