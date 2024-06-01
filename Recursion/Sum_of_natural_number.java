faf7b39da200d0c573e4f5b6583f1f28de275e25
itsdevaditya <adigoyl05@gmail.com>
2024-06-01 11:58:08 +0530

    try to make sum of first natural numbers

--- Recursion/Sum_of_natural_number.java
+++ Recursion/Sum_of_natural_number.java
@@ -2,21 +2,10 @@ package Recursion;
 
 import java.util.Scanner;
 
-public class print_number {
-    public static void print_num(int n) {
-        if (n == 0) {
-            return;
-        }
-        System.out.println(n);
-        print_num(n - 1);
-        
-    }
-
+public class Sum_of_natural_number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter any number : ");
         int n = sc.nextInt();
-
-        print_num(n);
     }
 }
