546a47811c5d25a1cad778b64d384dadbe132534
itsdevaditya <adigoyl05@gmail.com>
2024-06-01 11:46:52 +0530

    finally insertion sort code run

--- Shorting/insertion_sort.java
+++ Shorting/insertion_sort.java
@@ -5,6 +5,7 @@ public class insertion_sort {
         for (int i = 0; i < arry.length; i++) {
             System.out.print(arry[i] + " ");
         }
+
     }
 
     public static void main(String[] args) {
@@ -12,15 +13,20 @@ public class insertion_sort {
 
         int[] arr = { 4, 2, 5, 27, 1 };
         System.out.println("unsorted array : ");
-        printArray(arr);}
+        printArray(arr);
+        for (int i = 0; i < arr.length - 1; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            // placement
+            arr[j + 1] = current;
 
-    for(int i = 0;i<arr.length-1;i++){
-    int current = arr[i];
-    int j = i - 1;
-    while(j>=0  && current  = arr [j]){
-        arr[j+1] = arr[i];
+        }
+        System.out.println();
+        System.out.println("Shorted array by using insertion sort  ");
+        printArray(arr);
     }
-
 }
-
-}}
