28c67c086971a346f657a7c5f071af829e79296d
itsdevaditya <adigoyl05@gmail.com>
2024-06-01 11:33:57 +0530

    print reverse number using recursion function

--- Shorting/insertion_sort.java
+++ Shorting/insertion_sort.java
@@ -6,6 +6,7 @@ public class insertion_sort {
             System.out.print(arry[i] + " ");
         }
     }
+
     public static void main(String[] args) {
         // sorting using selection sort
 
@@ -13,12 +14,13 @@ public class insertion_sort {
         System.out.println("unsorted array : ");
         printArray(arr);}
 
-        for ( int i = 0 ; i < arr.length ; i++)
-        {
-            int current = arr[i];
-            int j = i -1;
-            while (j > = 0 && current ) {
-                
-            }
-        }
+    for(int i = 0;i<arr.length-1;i++){
+    int current = arr[i];
+    int j = i - 1;
+    while(j>=0  && current  = arr [j]){
+        arr[j+1] = arr[i];
+    }
+
 }
+
+}}
