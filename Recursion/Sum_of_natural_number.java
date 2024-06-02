e9b733fa384025c6c5751275f13af5b562f9a35a
itsdevaditya <adigoyl05@gmail.com>
2024-06-02 19:09:59 +0530

    sum of natural number by using recursion

--- Recursion/Sum_of_natural_number.java
+++ Recursion/Sum_of_natural_number.java
@@ -3,9 +3,25 @@ package Recursion;
 import java.util.Scanner;
 
 public class Sum_of_natural_number {
+
+    public static int sum_numb(int n, int sum) {
+
+        if (n == 0) {
+            sum += n;
+            System.out.println(sum);
+            return 0;
+
+        }
+        sum += n;
+        sum_numb(n - 1, sum);
+        return sum;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter any number : ");
         int n = sc.nextInt();
+        sum_numb(n, 0);
+        // System.out.println(sum_numb(n , 0));
     }
 }
