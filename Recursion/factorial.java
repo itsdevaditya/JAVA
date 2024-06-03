ce760f70775bf50de76d29f8efcd4618d838f4a7
itsdevaditya <adigoyl05@gmail.com>
2024-06-03 11:33:25 +0530

    factorial code

---
+++ Recursion/factorial.java
@@ -0,0 +1,11 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the ");
+        int n = sc.nextInt();
+    }
+}
