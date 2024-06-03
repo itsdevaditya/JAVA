2913a3f8c093a8a83225cc51d5a85fa4c7b7ba63
itsdevaditya <adigoyl05@gmail.com>
2024-06-03 11:55:45 +0530

    Factorial by using recursion

--- Recursion/factorial.java
+++ Recursion/factorial.java
@@ -3,9 +3,19 @@ package Recursion;
 import java.util.Scanner;
 
 public class factorial {
+    public static int fact(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int fac = n * (fact(n - 1));
+        return fac;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the ");
+        System.out.print("Enter any number : ");
         int n = sc.nextInt();
+        System.out.println("Factorial of " + n + "  is : " + fact(n));
+
     }
 }
