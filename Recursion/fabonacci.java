fba23cf98fb67375fc8129e359636770a6a5f2f9
itsdevaditya <adigoyl05@gmail.com>
2024-06-04 20:49:58 +0530

    fabonacci code using recursion

---
+++ Recursion/fabonacci.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class fabonacci {
+    public static void fab(int a, int b, int n) {
+        if (n == 0) {
+            return ;
+            
+        }
+        int c = a + b;
+        System.out.println(c);
+        fab(b, c, n - 1);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number : ");
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        System.out.print(a);
+        System.out.print(b);
+        fab(a, b, n - 2);
+
+    }
+}
