3ff599517a334f9ae66d590295dd6a632a1cf9c4
itsdevaditya <adigoyl05@gmail.com>
2024-06-04 20:54:01 +0530

    organise for linkdin

--- Recursion/fabonacci.java
+++ Recursion/fabonacci.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class fabonacci {
     public static void fab(int a, int b, int n) {
         if (n == 0) {
-            return ;
-            
+            return;
+
         }
         int c = a + b;
-        System.out.println(c);
+        System.out.print(c + " ");
         fab(b, c, n - 1);
 
     }
@@ -20,8 +20,9 @@ public class fabonacci {
         int n = sc.nextInt();
         int a = 0;
         int b = 1;
-        System.out.print(a);
-        System.out.print(b);
+        System.out.println("Fabonacci series of " + n + " numbers ");
+        System.out.print(a + " ");
+        System.out.print(b + " ");
         fab(a, b, n - 2);
 
     }
