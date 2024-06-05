6c6787a321f56ed1fb9c3599b2ce76ad088b328c
itsdevaditya <adigoyl05@gmail.com>
2024-06-05 12:12:33 +0530

    print x^n having T.C == log(n)

--- Recursion/print_XpowerN.java
+++ Recursion/print_XpowerN.java
@@ -2,17 +2,28 @@ package Recursion;
 
 import java.util.Scanner;
 
-public class print_XpowerN{
-    public static int calculate_pow(int x , int n , int product){
+public class print_XpowerN {
+    public static int calculate_pow(int x, int n, int product) {
         if (n == 0) {
-            return 1;    
+            return 1;
         }
         if (x == 0) {
-            return 0; 
-        }  
-        product = x*calculate_pow(x, n-1, product);
-        return product;
+            return 0;
+        }
+        //  Having time complexity = O(log(n))
+        if (n % 2 == 0) {
+            product = calculate_pow(x, n / 2, product) * calculate_pow(x, n / 2, product);
+            return product;
+        } else {
+            product = calculate_pow(x, n / 2, product) * calculate_pow(x, n / 2, product) * x;
+            return product;
+        }
     }
+    // }     Having time complexity = O(n)
+    // product = x*calculate_pow(x, n-1, product);
+    // return product;
+    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of x : ");
@@ -21,5 +32,5 @@ public class print_XpowerN{
         int n = sc.nextInt();
         System.out.println("Value  of X ^ N is : " + calculate_pow(x, n, 1));
     }
-    
+
 }
