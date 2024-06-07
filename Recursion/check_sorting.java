0b604611d064cca0c57897991571d905048ace12
itsdevaditya <adigoyl05@gmail.com>
2024-06-07 09:29:12 +0530

    check sorting in array

---
+++ Recursion/check_sorting.java
@@ -0,0 +1,42 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class check_sorting {
+    public static boolean a;
+
+    public static void check_sort(int arr[], int indx) {
+        if (indx == arr.length - 1) {
+            if (a == true) {
+                System.out.println("Given  array is  sorted ");
+                return;
+            } else {
+                System.out.println("Given  array is not sorted ");
+                return;
+
+            }
+        }
+        if (arr[indx] < arr[indx + 1]) {
+            a = true;
+        } else {
+            a = false;
+
+        }
+        check_sort(arr, indx + 1);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter the length of array : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter " + i + " element of array : ");
+            arr[i] = sc.nextInt();
+        }
+        check_sort(arr, 0);
+
+    }
+
+}
---
+++ Recursion/first_and_last_index.java
@@ -0,0 +1,39 @@
+package Recursion;
+
+import java.util.Scanner;
+
+import Learn_String.reverse_string;
+
+public class first_and_last_index {
+   public static int first = -1;
+   public static int last = -1;
+
+   public static void first_last_index(String str, int indx, char element) {
+
+      if (indx == str.length() - 1) {
+         System.out.println("Tirst index of element is : " + first);
+         System.out.println("last index of element is : " + last);
+         return;
+
+      }
+      if (str.charAt(indx) == element) {
+         if (first == -1) {
+            first = indx;
+         } else {
+            last = indx;
+         }
+      }
+      first_last_index(str, indx + 1, element);
+   }
+
+   public static void main(String[] args) {
+
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter your String ");
+      String str = sc.nextLine();
+      int len = 0;
+      System.out.print("Enter the charcter for finding its first and last index : ");
+      char element = sc.next().charAt(0);
+      first_last_index(str, len, element);
+   }
+}
