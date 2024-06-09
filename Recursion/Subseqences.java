5e3ba7359feef4b9f6eca1ee07e6b73a23a68037
itsdevaditya <adigoyl05@gmail.com>
2024-06-09 10:50:43 +0530

    subseqence code

---
+++ Recursion/Subseqences.java
@@ -0,0 +1,29 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Subseqences {
+    public static void print_subseqences(String str, int indx, String newString) {
+        if (indx == str.length()) {
+            System.out.println(newString);
+            return; 
+        }
+        char currentchar = str.charAt(indx);
+
+        // to be
+        print_subseqences(str, indx + 1, newString + currentchar);
+
+        // not to be
+        print_subseqences(str, indx + 1, newString);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your String : ");
+        String str = sc.nextLine();
+        System.out.println("Your Subseqences of " + str + " is : ");
+        print_subseqences(str, 0, "");
+
+    }
+
+}
