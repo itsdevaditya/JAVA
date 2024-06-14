dde942cbdac04500aa2fb188c0797d42c0756393
itsdevaditya <adigoyl05@gmail.com>
2024-06-14 20:15:16 +0530

    inheritence

---
+++ OOPS/inheritence.java
@@ -0,0 +1,23 @@
+package OOPS;
+
+class Animal {
+    public void walk() {
+        System.out.println("Animals are walk on one or more than one leg ");
+    }
+}
+
+class Cow extends Animal {
+    public void legs() {
+        System.out.println("cow has 4 legs ");
+    }
+}
+
+public class inheritence {
+    public static void main(String[] args) {
+        Cow c1 = new Cow();
+        c1.walk();
+        c1.legs();
+
+    }
+
+}
