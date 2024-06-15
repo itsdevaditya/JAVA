8afd6cd92dfe1c8db656bcba67d532f23d0802c8
itsdevaditya <adigoyl05@gmail.com>
2024-06-15 13:10:50 +0530

    first time create linklist

---
+++ collection_framework/LinkList.java
@@ -0,0 +1,50 @@
+package collection_framework;
+
+public class LinkList {
+    Node head ;
+    class Node {
+        String data ;
+        Node next ;
+
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    //  add first 
+    public void addFirst(String data){
+        Node newnode = new Node(data);
+        if (head == null) {
+            head = newnode; 
+            return;
+        }
+        newnode.next = head;
+        head = newnode;
+    }
+
+    // print linklist
+    public void printlinklist(){
+        if (head ==null) {
+            System.out.println("list is empty");
+            return;
+            
+        }
+        Node currnode = head;
+        while (currnode != null) {
+            System.out.print(currnode.data + " --> ");
+            currnode = currnode.next;
+        }
+        System.out.println("null");
+    }
+
+
+    public static void main(String[] args) {
+        LinkList list = new LinkList();
+        list.addFirst("linklist");
+        list.addFirst("is");
+        list.addFirst("this");
+        list.printlinklist();
+
+        
+    }
+}
