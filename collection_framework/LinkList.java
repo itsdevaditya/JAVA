28fe7d96ec6a2d384da9370080532a9c40b85ab9
itsdevaditya <adigoyl05@gmail.com>
2024-06-16 21:43:59 +0530

    simple linklist code

--- collection_framework/LinkList.java
+++ collection_framework/LinkList.java
@@ -1,33 +1,48 @@
 package collection_framework;
 
 public class LinkList {
-    Node head ;
-    class Node {
-        String data ;
-        Node next ;
+    Node head;
 
-        Node(String data){
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
             this.data = data;
             this.next = null;
         }
     }
-    //  add first 
-    public void addFirst(String data){
+
+    // add first
+    public void addFirst(String data) {
         Node newnode = new Node(data);
         if (head == null) {
-            head = newnode; 
+            head = newnode;
             return;
         }
         newnode.next = head;
         head = newnode;
     }
 
+    public void addlast(String data) {
+        Node newnode = new Node(data);
+        if (head == null) {
+            head = newnode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newnode;
+    }
+
     // print linklist
-    public void printlinklist(){
-        if (head ==null) {
+    public void printlinklist() {
+        if (head == null) {
             System.out.println("list is empty");
             return;
-            
+
         }
         Node currnode = head;
         while (currnode != null) {
@@ -37,14 +52,14 @@ public class LinkList {
         System.out.println("null");
     }
 
-
     public static void main(String[] args) {
         LinkList list = new LinkList();
         list.addFirst("linklist");
         list.addFirst("is");
         list.addFirst("this");
+        list.addlast("list");
+        list.addFirst("link");
         list.printlinklist();
 
-        
     }
 }
---
+++ collection_framework/linklist_frame.java
@@ -0,0 +1,24 @@
+package collection_framework;
+import java.util.*;
+
+public class linklist_frame {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(5);
+        list.add(7);
+        list.add(3);
+        list.add(8);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+        for(int i = 0 ; i < list.size() ; i++){
+            if (list.get(i) == 7) {
+                System.out.println("Index of 7 is : " + i );
+                return;
+            }
+        }
+    }
+    
+}
+
