2cab3acbbb58be88d19f3b51d4a20b21a7efa51e
itsdevaditya <adigoyl05@gmail.com>
2024-06-19 09:20:47 +0530

    Delete nth node from last in linklist

---
+++ collection_framework/find_index_from_last.java
@@ -0,0 +1,19 @@
+package collection_framework;
+
+import java.util.LinkedList;
+
+public class find_index_from_last {
+    public static void main(String[] args) {
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+        LinkedList<Integer> list = new LinkedList();
+        
+        list.add(1);
+        list.add(5);
+        list.add(7);
+        list.add(3);
+        list.add(8);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+    }
+}
