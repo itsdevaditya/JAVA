f21b09aa03c2ccdec42aeacabecf29810145da42
itsdevaditya <adigoyl05@gmail.com>
2024-06-21 21:58:45 +0530

    code of finding indwx from last

--- collection_framework/find_index_from_last.java
+++ collection_framework/find_index_from_last.java
@@ -1,12 +1,13 @@
 package collection_framework;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class find_index_from_last {
     public static void main(String[] args) {
         @SuppressWarnings({ "rawtypes", "unchecked" })
         LinkedList<Integer> list = new LinkedList();
-        
+
         list.add(1);
         list.add(5);
         list.add(7);
@@ -15,5 +16,14 @@ public class find_index_from_last {
         list.add(2);
         list.add(3);
         System.out.println(list);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the nth number ");
+        int n = sc.nextInt();
+
+        list.size();
+        int index = list.size() - n - 1;
+        System.out.println("Index of " + n + "th element from last is " + index);
+
     }
 }
